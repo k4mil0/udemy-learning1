fb04bdd4d15c0169446c58a16eca85e75db9ab3e
Kamil <kamilwitekpir2@gmail.com>
2018-08-17 12:19:50 +0200

    Add files via upload

---
+++ Nowy folder/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	$(".navbar").click(function(){
+		$(".navbar").css({"background-Color": "#66CDAA"});
+		$(this).css({"background-Color":"#329a77" , "border-radius": "5px 50px"});
+	});
+});
